f42f60d416826a68283fa0f21ffd8dc7da666eec
KyalSinLinLett <alexsin0012@gmail.com>
2023-07-05 14:45:57 +0630

    update websocket endpoint

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let remoteStream;
 let peerConnection;
 
 let socket = new WebSocket(
-    "wss://fathomless-dusk-11609.herokuapp.com/socket/websocket"
+    "wss://sigsvr.onrender.com/socket/websocket"
 );
 let joinStatus = false;
 
